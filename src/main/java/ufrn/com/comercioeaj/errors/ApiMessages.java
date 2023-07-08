57a588a470ccb6a4912062ada4a7e306cf3e119b
Ana Camilly <camillyguedes2@gmail.com>
2023-07-08 01:56:19 -0300

    ApiMessages

---
+++ src/main/java/ufrn/com/comercioeaj/errors/ApiMessages.java
@@ -0,0 +1,16 @@
+package ufrn.com.comercioeaj.errors;
+
+public final class ApiMessages {
+    public static final String ERRO_NOME = "Insira seu nome completo";
+    public static final String ERRO_NOME_SOCIAL = "Insira como você gostaria de ser chamado";
+
+    public static final String ERRO_EMAIL = "Insira seu e-mail";
+
+    public static final String ERRO_LOGIN = "Insira seu login";
+
+    public static final String ERRO_SENHA = "Insira sua senha";
+
+    public static final String ERRO_DATA_NASCIMENTO = "Insira sua data de nascimento";
+
+    //public static final String ___ = "***";
+}
--- src/main/java/ufrn/com/comercioeaj/models/Usuarios.java
+++ src/main/java/ufrn/com/comercioeaj/models/Usuarios.java
@@ -1,12 +1,15 @@
 package ufrn.com.comercioeaj.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import ufrn.com.comercioeaj.errors.ApiMessages;
 
 import java.sql.Date;
 import java.util.Collection;
@@ -20,16 +23,27 @@ public class Usuarios implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @NotBlank(message = ApiMessages.ERRO_NOME)
     String nome;
 
+    @NotBlank(message = ApiMessages.ERRO_NOME_SOCIAL)
     String nomeSocial;
 
+    @NotBlank(message = ApiMessages.ERRO_EMAIL)
     @Column(unique=true)
     String email;
+
+    @NotBlank(message = ApiMessages.ERRO_LOGIN)
     @Column(unique=true)
     String login;
+
+    @NotBlank(message = ApiMessages.ERRO_SENHA)
     String senha;
+
+    @NotNull(message = ApiMessages.ERRO_DATA_NASCIMENTO)
     Date dataNascimento;
+
     Date dataCadastro;
     Boolean isVendedor;
     String imagemUri;
